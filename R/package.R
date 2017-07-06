b65224aac88799449a78f3a8cf98a6637385984f
Thomas J. Leeper <thosjleeper@gmail.com>
2017-07-06 16:23:21 +0200

    initial commit

---
+++ R/package.R
@@ -0,0 +1,9 @@
+#' @name aws.dynamodb-package
+#' @title aws.dynamodb
+#' @aliases aws.dynamodb-package aws.dynamodb
+#' @docType package
+#' @description A simple client package for AWS Dynamo DB.
+#' @details A simple client package for AWS Dynamo DB.
+#' @author Thomas J. Leeper <thosjleeper@gmail.com>
+#' @keywords package 
+NULL
