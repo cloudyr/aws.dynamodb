917f2996baa4442c5c4759083832c63abe6b0e17
Thomas J. Leeper <thosjleeper@gmail.com>
2018-04-14 17:08:07 +0100

    cleanup http code & fix session_token arg

--- R/http.R
+++ R/http.R
@@ -1,11 +1,15 @@
 #' @title Execute Dynamo DB API Request
 #' @description This is the workhorse function to execute calls to the Dynamo DB API.
 #' @param verb A character string specifying an HTTP verb for the operation.
+#' @param headers A list of headers to pass to the HTTP request.
+#' @param body A list of body (JSON) arguments.
+#' @param target An API endpoint.
+#' @param verbose A logical indicating whether to be verbose. Default is given by \code{options("verbose")}.
 #' @param region A character string containing an AWS region. If missing, the default \dQuote{us-east-1} is used.
 #' @param key A character string containing an AWS Access Key ID. See \code{\link[aws.signature]{locate_credentials}}.
 #' @param secret A character string containing an AWS Secret Access Key. See \code{\link[aws.signature]{locate_credentials}}.
 #' @param session_token Optionally, a character string containing an AWS temporary Session Token. See \code{\link[aws.signature]{locate_credentials}}.
-#' @param ... Additional arguments passed to \code{\link[httr]{GET}}.
+#' @param \dots Additional arguments passed to \code{\link[httr]{GET}}.
 #' @return If successful, a named list. Otherwise, a data structure of class \dQuote{aws-error} containing any error message(s) from AWS and information about the request attempt.
 #' @details This function constructs and signs an Dynamo DB API request and returns the results thereof, or relevant debugging information in the case of error.
 #' @author Thomas J. Leeper
@@ -17,14 +21,24 @@
 dynamoHTTP <- 
 function(
   verb = "GET",
+  headers = list(),
   body = NULL,
   target,
-  region = Sys.getenv("AWS_DEFAULT_REGION","us-east-1"), 
+  verbose = getOption("verbose", FALSE),
+  region = Sys.getenv("AWS_DEFAULT_REGION","us-east-1"),
   key = NULL, 
   secret = NULL, 
   session_token = NULL,
   ...
 ) {
+    # locate and validate credentials
+    credentials <- locate_credentials(key = key, secret = secret, session_token = session_token, region = region, verbose = verbose)
+    key <- credentials[["key"]]
+    secret <- credentials[["secret"]]
+    session_token <- credentials[["session_token"]]
+    region <- credentials[["region"]]
+    
+    # generate request signature
     d_timestamp <- format(Sys.time(), "%Y%m%dT%H%M%SZ", tz = "UTC")
     url <- paste0("dynamodb.",region,".amazonaws.com")
     Sig <- signature_v4_auth(
@@ -39,16 +53,19 @@ function(
            request_body = if (length(body)) jsonlite::toJSON(body, auto_unbox = TRUE) else "",
            key = key, 
            secret = secret,
-           session_token = session_token)
-    headers <- list(`x-amz-date` = d_timestamp, 
-                    `x-amz-content-sha256` = Sig$BodyHash,
-                    `x-amz-target` = target,
-                    Authorization = Sig$SignatureHeader)
+           session_token = session_token,
+           verbose = verbose)
+    # setup request headers
+    headers[["x-amz-date"]] <- d_timestamp
+    headers[["x-amz-target"]] <- target
+    headers[["x-amz-content-sha256"]] <- Sig$BodyHash
+    headers[["Authorization"]] <- Sig[["SignatureHeader"]]
     if (!is.null(session_token) && session_token != "") {
         headers[["x-amz-security-token"]] <- session_token
     }
     H <- do.call(add_headers, headers)
     
+    # execute request
     if (verb == "GET") {
         r <- GET(url, H, body = body, encode = "json", ...)
     } else if (verb == "POST") {
